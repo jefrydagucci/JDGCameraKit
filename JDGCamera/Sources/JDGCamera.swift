92f1813d7e85c700c77a7df9c0e61742645d6673
Jefry <jefrydagucci@ahloo.com>
2017-03-03 11:01:40 +0700

    add camera controller

---
+++ JDGCamera/Sources/JDGCamera.swift
@@ -0,0 +1,13 @@
+//
+//  JDGCamera.swift
+//  JDGCamera
+//
+//  Created by Jefry Da Gucci on 3/3/17.
+//  Copyright © 2017 jefrydagucci. All rights reserved.
+//
+
+import UIKit
+
+class JDGCamera: UIViewController {
+
+}
