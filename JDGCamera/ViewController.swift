295099fb2384a53c9c40e3ce1f3dbbb6b6e5361f
Jefry <jefrydagucci@ahloo.com>
2017-03-03 10:55:15 +0700

    config project and dependency

---
+++ JDGCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JDGCamera
+//
+//  Created by Jefry Da Gucci on 3/3/17.
+//  Copyright © 2017 jefrydagucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'JDGCamera' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'TransitionTreasury'
+pod 'DBCamera'
+pod 'LLSimpleCamera'
+pod 'SDRecordButton'
+pod 'IoniconsSwift', '~> 2.1.4'
+
+  # Pods for JDGCamera
+
+end
