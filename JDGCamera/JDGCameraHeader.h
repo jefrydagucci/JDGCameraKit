68941b28fcbe2b06564220315e4d5034b6f35440
Jefry <jefrydagucci@ahloo.com>
2017-03-03 10:59:31 +0700

    config header

---
+++ JDGCamera/JDGCameraHeader.h
@@ -0,0 +1,16 @@
+//
+//  JDGCameraHeader.h
+//  JDGCamera
+//
+//  Created by Jefry Da Gucci on 3/3/17.
+//  Copyright © 2017 jefrydagucci. All rights reserved.
+//
+
+#ifndef JDGCameraHeader_h
+#define JDGCameraHeader_h
+
+#import <LLSimpleCamera/LLSimpleCamera.h>
+#import <SDRecordButton/SDRecordButton.h>
+
+
+#endif /* JDGCameraHeader_h */
