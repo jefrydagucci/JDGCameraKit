d2c507546b571a821df2f10d2c2320a714ca6c4b
Jefry <jefrydagucci@ahloo.com>
2017-03-06 15:19:47 +0700

    add camerakit for creating framework

---
+++ JDGCameraKit/JDGCameraKit.h
@@ -0,0 +1,19 @@
+//
+//  JDGCameraKit.h
+//  JDGCameraKit
+//
+//  Created by Jefry Da Gucci on 3/6/17.
+//  Copyright © 2017 jefrydagucci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JDGCameraKit.
+FOUNDATION_EXPORT double JDGCameraKitVersionNumber;
+
+//! Project version string for JDGCameraKit.
+FOUNDATION_EXPORT const unsigned char JDGCameraKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDGCameraKit/PublicHeader.h>
+
+
